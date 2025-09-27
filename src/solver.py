9aee2b50e274eb832f732f51a8ad9e7c34898ece
Yury Meshin <yury@Yurys-iMac.local>
2025-09-27 17:22:03 +0100

    minor refactoring

--- src/solver.py
+++ src/solver.py
@@ -1,14 +1,16 @@
-from typing import Optional
+from typing import Optional, Iterator
 from enum import Enum
 
 
-def map_index(coeffs: list[int], indices: list[int]) -> int:
-    assert len(coeffs) == len(indices), "Given coeffs and indices size do not match"
-    index = 0
-    for i in range(len(coeffs)):
-        index += coeffs[i] * indices[i]
-    return index
+def get_block_indices(left_corner: tuple[int, int], block_shape: tuple[int, int], side_len: int) -> list[int]:
+    ''' Returns flattened block p x q with left corner (x, y) in square grid of side_len x side_len '''
+    
+    assert all(0 <= left_corner[i] <= side_len - block_shape[i] for i in range(2)) and (min(block_shape) >= 1), \
+        f"Incorrect input {left_corner=} {block_shape=} {side_len=}"
 
+    offset = left_corner[0] * side_len + left_corner[1]
+    return [offset + r * side_len + c for r in range(block_shape[0]) for c in range(block_shape[1])]
+ 
 
 class CellStatus(Enum):
     EMPTY = 0
@@ -25,35 +27,44 @@ class GridCell():
         if not (bitmask is None):
             assert 0 <= bitmask <= default_bitmask, f"{bitmask=} must be between 0 and {default_bitmask}"
         self.slots = slots
-        self.bitmask = bitmask if bitmask else default_bitmask
+        self._bitmask = bitmask if bitmask else default_bitmask
+        self._options = self._get_options()
+
+    def _get_options(self) -> list[int]:
+        ''' Extracts 1-based values from bitmask '''
+        return [i + 1 for i in range(self.slots) if self._bitmask & (1 << i)]
 
     @property
     def options(self) -> list[int]:
-        return [i + 1 for i in range(self.slots) if self.bitmask & (1 << i)]
+        return self._options
+
+    @property
+    def bitmask(self) -> int:
+        return self._bitmask
 
     def __str__(self) -> str:
         return ",".join([str(v) for v in self.options])
     
     @property
     def status(self) -> CellStatus:
-        match self.bitmask.bit_count():
+        match self._bitmask.bit_count():
             case 0: return CellStatus.EMPTY
             case 1: return CellStatus.DETERMINED
             case _: return CellStatus.OPTIONAL
 
-    def set(self, bitmask: int):
+    def set(self, bitmask: int) -> None:
         default_bitmask = (1 << self.slots) - 1
         assert 0 <= bitmask <= default_bitmask, f"{bitmask=} must be between 0 and {default_bitmask}"
-        self.bitmask = bitmask
+        self._bitmask = bitmask
+        self._options = self._get_options()
     
-    def __iter__(self):
-        for v in self.options:
-            yield v
+    def __iter__(self) -> Iterator[int]:
+        return iter(self.options)
     
     def check_mask(self, mask: int) -> bool:
         ''' Checks if given cell possible options belongs to certain set defined by its bitmask '''
         assert 0 < mask < (1 << self.slots), f"Incorrect {mask=} given"
-        return (self.bitmask & ~mask) == 0
+        return (self._bitmask & ~mask) == 0
 
 
 class SudokuGrid():
@@ -64,10 +75,23 @@ class SudokuGrid():
         self.m = m
         self.n = n
         self.slots = m * n
-        self.grid = [GridCell(self.slots) for _ in range(self.slots ** 2)]
-        self.areas = [[map_index([self.slots, 1], [i, j]) for j in range(self.slots)] for i in range(self.slots)]
-        self.areas += [[map_index([self.slots, 1], [i, j]) for i in range(self.slots)] for j in range(self.slots)]
-        self.areas += [[map_index([self.slots * m, n, self.slots, 1], [i, j, r, s]) for r in range(m) for s in range(n)] for i in range(n) for j in range(m)]
+        self.grid: list[GridCell] = [GridCell(self.slots) for _ in range(self.slots ** 2)]
+        
+        self._areas: list[list[int]] = []
+        
+        # add rows
+        for i in range(self.slots):
+            self._areas.append(get_block_indices((i, 0), (1, self.slots), self.slots))
+        
+        # add columns
+        for i in range(self.slots):
+            self._areas.append(get_block_indices((0, i), (self.slots, 1), self.slots))
+        
+        # add blocks
+        for i in range(self.n):
+            for j in range(self.m):
+                self._areas.append(get_block_indices((i * self.m, j * self.n), (self.m, self.n), self.slots))
+
         self._is_valid = True
     
     @property
@@ -82,23 +106,24 @@ class SudokuGrid():
                 case CellStatus.DETERMINED: return str(cell)
                 case _: return "."
         
-        rows = self.slots
-        return "\n".join([" ".join([show_value(self.grid[map_index([rows, 1], [i, j])]) for j in range(rows)]) for i in range(rows)])
+        rows = [get_block_indices((i, 0), (1, self.slots), self.slots) for i in range(self.slots)]
+        return "\n".join([" ".join([show_value(self.grid[i]) for i in row]) for row in rows])
 
     def __repr__(self) -> str:
         cells = [str(cell) for cell in self.grid]
         mx = max(len(cell) for cell in cells)
-        rows = ["-" * ((mx + 1) * self.slots + 1)]
-        for i in range(self.slots):
+        lines = ["-" * ((mx + 1) * self.slots + 1)]
+        rows = [get_block_indices((i, 0), (1, self.slots), self.slots) for i in range(self.slots)]
+        for row in rows:
             vals = [""]
-            for j in range(self.slots):
-                vals.append(str(self.grid[map_index([self.slots, 1], [i, j])]).rjust(mx))
-            rows.append("|".join(vals) + "|")
-            rows.append("-" * ((mx + 1) * self.slots + 1))
-        return "\n".join(rows)
+            for i in row:
+                vals.append(str(self.grid[i]).rjust(mx))
+            lines.append("|".join(vals) + "|")
+            lines.append("-" * ((mx + 1) * self.slots + 1))
+        return "\n".join(lines)
 
     @classmethod
-    def read_board(cls, shape: tuple[int, int], board: str):
+    def read_board(cls, shape: tuple[int, int], board: str) -> "SudokuGrid":
         cells = board.split(",")
         assert len(cells) == (shape[0] * shape[1]) ** 2, "Given board does not match to dimensions"
         grid = cls(shape[0], shape[1])
@@ -108,37 +133,47 @@ class SudokuGrid():
                 grid[i] = GridCell(grid.slots, 1 << (int(val) - 1))
         return grid
     
-    def __setitem__(self, index: int, value: GridCell):
+    def __setitem__(self, index: int, value: GridCell) -> None:
         self.grid[index] = value
 
     def __getitem__(self, index: int) -> GridCell:
         return self.grid[index]
     
-    def __iter__(self):
-        for cell in self.grid:
-            yield cell
+    def __iter__(self) -> Iterator[GridCell]:
+        return iter(self.grid)
 
-    def reduce_options(self):
+    def reduce_options(self) -> None:
         ''' Eliminates potential values in given grid cells '''
 
-        for area in self.areas:
+        def get_subareas(area: list[int], mask: int) -> tuple[list[int], list[int]]:
+            ''' Returns area cells indices which could be covered by certain set of values defined by its bitmask '''
+            masked, unmasked = [], []
+            for i in area:
+                if self.grid[i].check_mask(mask):
+                    masked.append(i)
+                else:
+                    unmasked.append(i)
+            return masked, unmasked
+
+        def update_cells(selection: list[int], exclude_mask: int) -> None:
+            ''' Updates given selection cells by excluding some set defined by its bitmask  '''
+            for i in selection:
+                self.grid[i].set(self.grid[i].bitmask & ~exclude_mask)
+
+        for area in self._areas:
             masks = set(self.grid[i].bitmask for i in area)
+            if 0 in masks:
+                self._is_valid = False
+                return 
             for mask in masks:
-                # seeking all cells in area which options belong to certain subset described by bitmask
-                masked = [i for i in area if self.grid[i].check_mask(mask)]
+                masked, unmasked = get_subareas(area, mask)
                 if len(masked) == mask.bit_count():
-                    # if we found exact number of cell for given subset 
-                    # we should eliminate those values from remaing cells in this area
-                    for j in area:
-                        if not j in masked:
-                            self.grid[j].set(self.grid[j].bitmask & ~mask)
+                    update_cells(unmasked, mask) # eliminate appearances of set of values defined by its btimask
                 elif len(masked) > mask.bit_count():
-                    # if we found more then contradiction found and nothing to do there
-                    for j in area:
-                        self.grid[j] = GridCell(self.slots, 0)
+                    update_cells(area, (1 << self.slots) - 1) # if we found more matching cells then contradiction found
                     self._is_valid = False
     
-    def simplify(self):
+    def simplify(self) -> None:
         ''' Reducing potential grid cells options as long as possible '''
         if not self.is_valid:
             return # there is nothing to do there, we have found a contradiction
@@ -166,7 +201,7 @@ class SudokuSolver():
         self.block_shape = shape
         self.board = SudokuGrid.read_board(shape, board)
     
-    def solve(self):
+    def solve(self) -> None:
         self.board.simplify()
         if not self.board.is_valid:
             return # There is nothing to do there, contradiction has found
@@ -190,3 +225,4 @@ if __name__ == '__main__':
     sl = SudokuSolver((3, 3), "4,.,.,.,.,.,.,1,.,.,7,.,.,.,.,.,.,.,.,.,1,.,6,.,.,3,.,2,.,6,8,.,.,1,4,.,.,3,9,4,.,.,2,.,.,.,.,.,.,7,.,.,9,3,.,.,.,.,.,8,4,2,.,3,.,.,.,.,.,.,8,9,8,.,4,.,.,2,.,.,1")
     sl.solve()
     print(sl.board)
+    print(repr(sl.board))
