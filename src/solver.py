e193832119245f38959193b97634d7ce2d70f611
YuryMeshin <138002957+YuryMeshin@users.noreply.github.com>
2025-09-27 10:56:07 +0100

    Board validation optimized
    Co-authored-by: Yury Meshin <yury@localhost.localdomain>

--- src/solver.py
+++ src/solver.py
@@ -17,39 +17,43 @@ class CellStatus(Enum):
 
 
 class GridCell():
-    ''' Cell for sudoku grid'''
+    ''' Cell for sudoku grid, represented as bitmask of given slots size '''
 
-    def __init__(self, slots: int, value: Optional[int] = None):
+    def __init__(self, slots: int, bitmask: Optional[int] = None):
         assert 1 < slots < 17, f"{slots=} must be between 2 and 16 exclusively" 
-        if value:
-            assert 0 <= value < (1 << slots), f"{value=} must be between 0 and {(1 << slots) - 1}"
+        default_bitmask = (1 << slots) - 1
+        if not (bitmask is None):
+            assert 0 <= bitmask <= default_bitmask, f"{bitmask=} must be between 0 and {default_bitmask}"
         self.slots = slots
-        self.value = value if value else (1 << slots) - 1
+        self.bitmask = bitmask if bitmask else default_bitmask
 
     @property
     def options(self) -> list[int]:
-        return [i + 1 for i in range(self.slots) if self.value & (1 << i)]
+        return [i + 1 for i in range(self.slots) if self.bitmask & (1 << i)]
 
     def __str__(self) -> str:
         return ",".join([str(v) for v in self.options])
     
     @property
     def status(self) -> CellStatus:
-        match len(self.options):
+        match self.bitmask.bit_count():
             case 0: return CellStatus.EMPTY
             case 1: return CellStatus.DETERMINED
             case _: return CellStatus.OPTIONAL
 
-    def set(self, value: int):
-        self.value = value
+    def set(self, bitmask: int):
+        default_bitmask = (1 << self.slots) - 1
+        assert 0 <= bitmask <= default_bitmask, f"{bitmask=} must be between 0 and {default_bitmask}"
+        self.bitmask = bitmask
     
     def __iter__(self):
         for v in self.options:
             yield v
     
     def check_mask(self, mask: int) -> bool:
+        ''' Checks if given cell possible options belongs to certain set defined by its bitmask '''
         assert 0 < mask < (1 << self.slots), f"Incorrect {mask=} given"
-        return (self.value & ~mask) == 0
+        return (self.bitmask & ~mask) == 0
 
 
 class SudokuGrid():
@@ -64,6 +68,7 @@ class SudokuGrid():
         self.areas = [[map_index([self.slots, 1], [i, j]) for j in range(self.slots)] for i in range(self.slots)]
         self.areas += [[map_index([self.slots, 1], [i, j]) for i in range(self.slots)] for j in range(self.slots)]
         self.areas += [[map_index([self.slots * m, n, self.slots, 1], [i, j, r, s]) for r in range(m) for s in range(n)] for i in range(n) for j in range(m)]
+        self.is_valid = True
     
     def __str__(self) -> str:
 
@@ -98,17 +103,7 @@ class SudokuGrid():
                 assert 1 <= int(val) <= grid.slots, f"Given value {val} is out of range"
                 grid[i] = GridCell(grid.slots, 1 << (int(val) - 1))
         return grid
-        
-    @property
-    def is_valid(self):
-
-        def validate_area(area: list[int]) -> bool:
-            vals = set().union(*[set(self.grid[i].options) for i in area])
-            exact_vals = [str(self.grid[i]) for i in area if self.grid[i].status == CellStatus.DETERMINED]
-            return (vals == set(range(1, self.slots + 1))) & (len(exact_vals) == len(set(exact_vals)))
-        
-        return all([validate_area(area) for area in self.areas])
-
+    
     def __setitem__(self, index: int, value: GridCell):
         self.grid[index] = value
 
@@ -120,25 +115,43 @@ class SudokuGrid():
             yield cell
 
     def reduce_options(self):
+        ''' Eliminates potential values in given grid cells '''
+
         for area in self.areas:
             for mask in range(1, 1 << self.slots):
+                # seeking all cells in area which options belong to certain subset described by bitmask
                 masked = [i for i in area if self.grid[i].check_mask(mask)]
                 if len(masked) == mask.bit_count():
+                    # if we found exact number of cell for given subset 
+                    # we should eliminate those values from remaing cells in this area
                     for j in area:
                         if not j in masked:
-                            self.grid[j].set(self.grid[j].value & ~mask)
+                            self.grid[j].set(self.grid[j].bitmask & ~mask)
+                elif len(masked) > mask.bit_count():
+                    # if we found more then contradiction found and nothing to do there
+                    for j in area:
+                        self.grid[j] = GridCell(self.slots, 0)
+                    self.is_valid = False
     
     def simplify(self):
-        options = sum([len(x.options) for x in self.grid])
+        ''' Reducing potential grid cells options as long as possible '''
+        if not self.is_valid:
+            return # there is nothing to do there, we have found a contradiction
+        options = sum([len(cell.options) for cell in self.grid])
         while True:
             self.reduce_options()
+            if not self.is_valid:
+                return # there is nothing to do there, we have found a contradiction
             new_options = sum([len(x.options) for x in self.grid])
             if new_options == options:
                 break
             options = new_options
     
     def flatten(self) -> str:
-        return ",".join([str(cell) if cell.status == CellStatus.DETERMINED else "." for cell in self.grid])
+        if self.is_valid:
+            return ",".join([str(cell) if cell.status == CellStatus.DETERMINED else "." for cell in self.grid])
+        else:
+            return ""
 
      
 class SudokuSolver():
@@ -150,10 +163,12 @@ class SudokuSolver():
     
     def solve(self):
         self.board.simplify()
+        if not self.board.is_valid:
+            return # There is nothing to do there, contradiction has found
         while not all(cell.status == CellStatus.DETERMINED for cell in self.board):
             for idx in range(len(self.board.grid)):
                 if self.board[idx].status != CellStatus.DETERMINED:
-                    value = 0
+                    cell_mask = 0
                     for v in self.board.grid[idx]:
                         new_grid = SudokuGrid.read_board(self.block_shape, self.board.flatten())
                         new_grid[idx] = GridCell(new_grid.slots, 1 << (v - 1))
@@ -162,6 +177,11 @@ class SudokuSolver():
                             if all(cell.status == CellStatus.DETERMINED for cell in new_grid):
                                 self.board = new_grid
                                 return
-                            value += 1 << (v - 1)
-                    self.board[idx] = GridCell(self.board.slots, value)
+                            cell_mask += 1 << (v - 1)
+                    self.board[idx] = GridCell(self.board.slots, cell_mask)
                     self.board.simplify()
+
+if __name__ == '__main__':
+    sl = SudokuSolver((3, 3), "4,.,.,.,.,.,.,1,.,.,7,.,.,.,.,.,.,.,.,.,1,.,6,.,.,3,.,2,.,6,8,.,.,1,4,.,.,3,9,4,.,.,2,.,.,.,.,.,.,7,.,.,9,3,.,.,.,.,.,8,4,2,.,3,.,.,.,.,.,.,8,9,8,.,4,.,.,2,.,.,1")
+    sl.solve()
+    print(sl.board)
